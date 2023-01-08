d537348a4009ddc330e9594b025d272f58ed711b
harun <harun@HARUN-TURUNC>
2023-01-08 23:10:08 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace nethesabı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTurkceDogru = new System.Windows.Forms.TextBox();
+            this.txtTurkceNet = new System.Windows.Forms.TextBox();
+            this.txtTurkceYanlis = new System.Windows.Forms.TextBox();
+            this.txtMatYanlis = new System.Windows.Forms.TextBox();
+            this.txtMatNet = new System.Windows.Forms.TextBox();
+            this.txtMatDogru = new System.Windows.Forms.TextBox();
+            this.txtFizikYanlis = new System.Windows.Forms.TextBox();
+            this.txtFizikNet = new System.Windows.Forms.TextBox();
+            this.txtFizikDogru = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTurkceDogru
+            // 
+            this.txtTurkceDogru.Location = new System.Drawing.Point(250, 160);
+            this.txtTurkceDogru.Name = "txtTurkceDogru";
+            this.txtTurkceDogru.Size = new System.Drawing.Size(34, 20);
+            this.txtTurkceDogru.TabIndex = 0;
+            // 
+            // txtTurkceNet
+            // 
+            this.txtTurkceNet.Location = new System.Drawing.Point(330, 160);
+            this.txtTurkceNet.Name = "txtTurkceNet";
+            this.txtTurkceNet.Size = new System.Drawing.Size(34, 20);
+            this.txtTurkceNet.TabIndex = 1;
+            // 
+            // txtTurkceYanlis
+            // 
+            this.txtTurkceYanlis.Location = new System.Drawing.Point(290, 160);
+            this.txtTurkceYanlis.Name = "txtTurkceYanlis";
+            this.txtTurkceYanlis.Size = new System.Drawing.Size(34, 20);
+            this.txtTurkceYanlis.TabIndex = 2;
+            // 
+            // txtMatYanlis
+            // 
+            this.txtMatYanlis.Location = new System.Drawing.Point(290, 186);
+            this.txtMatYanlis.Name = "txtMatYanlis";
+            this.txtMatYanlis.Size = new System.Drawing.Size(34, 20);
+            this.txtMatYanlis.TabIndex = 5;
+            // 
+            // txtMatNet
+            // 
+            this.txtMatNet.Location = new System.Drawing.Point(330, 186);
+            this.txtMatNet.Name = "txtMatNet";
+            this.txtMatNet.Size = new System.Drawing.Size(34, 20);
+            this.txtMatNet.TabIndex = 4;
+            // 
+            // txtMatDogru
+            // 
+            this.txtMatDogru.Location = new System.Drawing.Point(250, 186);
+            this.txtMatDogru.Name = "txtMatDogru";
+            this.txtMatDogru.Size = new System.Drawing.Size(34, 20);
+            this.txtMatDogru.TabIndex = 3;
+            // 
+            // txtFizikYanlis
+            // 
+            this.txtFizikYanlis.Location = new System.Drawing.Point(290, 212);
+            this.txtFizikYanlis.Name = "txtFizikYanlis";
+            this.txtFizikYanlis.Size = new System.Drawing.Size(34, 20);
+            this.txtFizikYanlis.TabIndex = 8;
+            // 
+            // txtFizikNet
+            // 
+            this.txtFizikNet.Location = new System.Drawing.Point(330, 212);
+            this.txtFizikNet.Name = "txtFizikNet";
+            this.txtFizikNet.Size = new System.Drawing.Size(34, 20);
+            this.txtFizikNet.TabIndex = 7;
+            // 
+            // txtFizikDogru
+            // 
+            this.txtFizikDogru.Location = new System.Drawing.Point(250, 212);
+            this.txtFizikDogru.Name = "txtFizikDogru";
+            this.txtFizikDogru.Size = new System.Drawing.Size(34, 20);
+            this.txtFizikDogru.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "D";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(329, 133);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(29, 13);
+            this.label.TabIndex = 11;
+            this.label.Text = "NET";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Türkçe";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(188, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Matematik";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Fizik";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 37);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFizikYanlis);
+            this.Controls.Add(this.txtFizikNet);
+            this.Controls.Add(this.txtFizikDogru);
+            this.Controls.Add(this.txtMatYanlis);
+            this.Controls.Add(this.txtMatNet);
+            this.Controls.Add(this.txtMatDogru);
+            this.Controls.Add(this.txtTurkceYanlis);
+            this.Controls.Add(this.txtTurkceNet);
+            this.Controls.Add(this.txtTurkceDogru);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTurkceDogru;
+        private System.Windows.Forms.TextBox txtTurkceNet;
+        private System.Windows.Forms.TextBox txtTurkceYanlis;
+        private System.Windows.Forms.TextBox txtMatYanlis;
+        private System.Windows.Forms.TextBox txtMatNet;
+        private System.Windows.Forms.TextBox txtMatDogru;
+        private System.Windows.Forms.TextBox txtFizikYanlis;
+        private System.Windows.Forms.TextBox txtFizikNet;
+        private System.Windows.Forms.TextBox txtFizikDogru;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
